a3a5868cdc2ffd260c79394ace4056c510963bb5
Joey Freund <joeyfreund@gmail.com>
2016-06-28 16:13:43 -0400

    create simple web-scraping script, as an example of async programming and using callbacks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-// Import the default export (with a local alias)
-import sayHello from './hello.js';
-// Import a named export
-import { one } from './hello.js';
+import request from "request";
+import cheerio from "cheerio";
 
-sayHello('Dan');
+function scrapeCatalogPage(url, onError, onData){
+
+  let itemToDataObject = (item) => {
+    let result = {};
+    ['sku', 'brand', 'name', 'image'].forEach(key => {
+      result[key] = cheerio(`meta[itemprop=${key}]`, item)[0].attribs.content;
+    });
+    return result;
+  }
+
+
+  request(url, function (error, response, body) {
+    if(error){
+      onError(error);
+    } else {
+      let itemId2data = {};
+
+      cheerio('div.browsing-product-item', body).each((_, item) => {
+        var itemData = itemToDataObject(item);
+        itemId2data[itemData['sku']] = itemData;
+      });
+
+      onData(itemId2data);
+
+    }
+
+  });
+
+}
+
+
+scrapeCatalogPage('https://www.ssense.com/en-ca/women/sale',
+  err  => console.log('ERROR!', err),
+  data => console.log('Got ' + Object.keys(data).length + ' of data items.', data)
+);
